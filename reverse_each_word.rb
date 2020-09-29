8aa7a0e1351f69f1b9f7332e98955f1d80c9aee7
John Exarhouleas <exarhouleasjohn@gmail.com>
2020-09-29 14:37:20 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,10 @@
+def reverse_each_word(string)
+  string.collect do |element|
+    element.to_a.collect do |letter|
+      for element.length
+        letter[0] 
+      end
+      
+    end
+  end
+end
